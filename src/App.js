81ec50c6ca4a77d1f290a9eaed9fa8feb531a1b3
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-03 20:48:55 +0000

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import {Chessboard} from 'react-chessboard'
 import {Chess} from 'chess.js'
-
+              
 function App() {
   const [game, setGame] = useState(new Chess());
 //Let's perform a function on the game state 
